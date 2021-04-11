ba1a0f451319ad2edb90f23e70b9576313b70f18
Paul Mölders <paulmoelders@gmail.com>
2021-04-11 14:40:32 +0200

    domain setup

--- lib/randarar-stack.ts
+++ lib/randarar-stack.ts
@@ -2,6 +2,9 @@ import * as cdk from "@aws-cdk/core";
 import * as nodeLambda from "@aws-cdk/aws-lambda-nodejs";
 import * as lambda from "@aws-cdk/aws-lambda";
 import * as apigateway from "@aws-cdk/aws-apigateway";
+import * as route53 from "@aws-cdk/aws-route53";
+import * as targets from "@aws-cdk/aws-route53-targets";
+import * as acm from "@aws-cdk/aws-certificatemanager";
 
 export class RandararStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
@@ -11,6 +14,9 @@ export class RandararStack extends cdk.Stack {
 }
 
 class RandararService extends cdk.Construct {
+  private hostedZoneName = "salmo.link";
+  private domain = "p.salmo.link";
+
   constructor(scope: cdk.Construct, id: string) {
     super(scope, id);
     const renderer = new nodeLambda.NodejsFunction(this, "RandararRenderer", {
@@ -34,9 +40,33 @@ class RandararService extends cdk.Construct {
         ),
       ],
     });
+
+    const salmoHostedZone = route53.HostedZone.fromLookup(
+      this,
+      this.hostedZoneName,
+      {
+        domainName: this.hostedZoneName,
+      }
+    );
+
+    const certificate = new acm.Certificate(this, "PreviewCertificate", {
+      domainName: this.domain,
+      validation: acm.CertificateValidation.fromDns(salmoHostedZone),
+    });
+
     const api = new apigateway.LambdaRestApi(this, "randarar", {
       handler: renderer,
       binaryMediaTypes: ["*/*"],
+      domainName: {
+        domainName: this.domain,
+        certificate: certificate,
+      },
+    });
+
+    new route53.ARecord(this, "CustomDomainAliasRecord", {
+      zone: salmoHostedZone,
+      recordName: this.domain,
+      target: route53.RecordTarget.fromAlias(new targets.ApiGateway(api)),
     });
 
     new cdk.CfnOutput(this, "API", {
